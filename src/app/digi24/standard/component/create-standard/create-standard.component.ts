9362489b5c6e32c8100573d2f31707947e6dbf3e
vinayaknode2017 <vinayaknode2017@gmail.com>
2018-12-27 22:01:03 +0530

    loader implemented fot create standard

--- src/app/digi24/standard/component/create-standard/create-standard.component.ts
+++ src/app/digi24/standard/component/create-standard/create-standard.component.ts
@@ -8,6 +8,8 @@ import { RootComponent } from '../../../../shared/roots/root.component';
 import { HttpResponseModel } from '../../model/http-response-model';
 import swal from 'sweetalert2';
 import { NgForm } from '@angular/forms';
+import { Ng4LoadingSpinnerService } from 'ng4-loading-spinner';
+import { timeout } from 'rxjs/operators';
 
 @Component({
   selector: 'app-create-standard',
@@ -24,6 +26,7 @@ export class CreateStandardComponent extends RootComponent implements OnInit {
     constructor(private standardService: StandardService,
                 private route: ActivatedRoute,
                 private location: Location,
+                private spinnerService: Ng4LoadingSpinnerService,
                 public globalService: GlobalService ) {
                   super(globalService);
                  }
@@ -34,19 +37,29 @@ export class CreateStandardComponent extends RootComponent implements OnInit {
     onSubmit(standardForm: NgForm) {
       this.loading = true;
   
+      this.spinnerService.show();
+
       this.standardService.createStandard(this.standardModel).subscribe(
         (data: HttpResponseModel<number>) => {
           if(data.isFaulted === false) {
-            swal({
-              position: 'center',
-              type: 'success',
-              title: data.responseMessage,
-              showConfirmButton: false,
-              timer: 1500
-            });
-            standardForm.reset();
+
+              this.spinnerService.hide();
+
+              swal({
+                position: 'center',
+                type: 'success',
+                title: data.responseMessage,
+                showConfirmButton: false,
+                timer: 1500
+              });
+  
+              standardForm.reset();              
+
             //this.location.back();
-          } else {
+             } else {
+
+            this.spinnerService.hide();
+
             swal({
               position: 'center',
               type: 'error',
@@ -57,6 +70,9 @@ export class CreateStandardComponent extends RootComponent implements OnInit {
           }
         },
         (failure) => {
+          
+          this.spinnerService.hide();
+
           swal({
             position: 'center',
             type: 'error',

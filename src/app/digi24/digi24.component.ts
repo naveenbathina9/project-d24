03a04824d5300be4e3a8cd539211771193a3159f
vinayaknode2017 <vinayaknode2017@gmail.com>
2018-12-29 00:14:19 +0530

    loader error rectified

--- src/app/digi24/digi24.component.ts
+++ src/app/digi24/digi24.component.ts
@@ -6,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./digi24.component.scss']
 })
 export class Digi24Component implements OnInit {
-loaderTemplate: string = `<img src="assets/iconfonts/ajax-loader.gif" />`;
 
   constructor() { }
 

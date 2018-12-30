d1bb9e4054ea641302f6e10448bdf430ff5c83e9
vinayaknode2017 <vinayaknode2017@gmail.com>
2018-12-30 17:45:40 +0530

    student list implementation

--- src/app/digi24/student/component/list-student/list-student.component.ts
+++ src/app/digi24/student/component/list-student/list-student.component.ts
@@ -3,29 +3,58 @@ import { StandardService } from '../../../standard/services/standard.service';
 import { StandardModel } from '../../../standard/model/standard-model';
 import { HttpResponseModel } from '../../models/http-response-model';
 import { Ng4LoadingSpinnerService } from 'ng4-loading-spinner';
+import { RootComponent } from '../../../../shared/roots/root.component';
+import { GlobalService } from '../../../../shared/services/global.service';
+import swal from 'sweetalert2';
+import { StudentService } from '../../services/student.service';
+import { StudentBasicData } from '../../models/student-basic-data';
 
 @Component({
   selector: 'app-list-student',
   templateUrl: './list-student.component.html',
   styleUrls: ['./list-student.component.scss'],
-  providers: [StandardService]
+  providers: [
+    StandardService,
+    StudentService
+  ]
 })
-export class ListStudentComponent implements OnInit {
+export class ListStudentComponent extends RootComponent implements OnInit {
   pageSize = 10;
   pageNumber = 1;
-  standards: StandardModel[];
+
+  standards: StandardModel[] = [];
+  students: StudentBasicData[] = [];
 
   constructor(private standardService: StandardService,
-              private spinnerService: Ng4LoadingSpinnerService) { }
+              private studentService: StudentService,
+              private spinnerService: Ng4LoadingSpinnerService,
+              private globalservice: GlobalService) { 
+                super(globalservice);
+              }
 
   ngOnInit() {
+    this.loadData();
+  }
+
+  onSelectionChange(standardId: string) {
     this.spinnerService.show();
-    this.standardService.getAllStandards().subscribe(
-      (data: StandardModel[]) => {
+    this.studentService.getStudentsByStandardId(standardId).subscribe(
+      data => {
         this.spinnerService.hide();
-        this.standards = data;
+        this.students = data.responseData;
+        console.log(this.students);
+      },
+      error => {
+        this.spinnerService.hide();
+        swal({
+          position: 'center',
+          type: 'error',
+          title: 'Error occured in server',
+          showConfirmButton: false,
+          timer: 1500
+        });
       }
-    )
+    );
   }
 
   createStudent() {
@@ -33,7 +62,26 @@ export class ListStudentComponent implements OnInit {
   }
 
   loadData() {
-    
+    this.spinnerService.show();
+    this.standardService.getAllStandards().subscribe(
+      (data: StandardModel[]) => {
+        this.spinnerService.hide();
+        this.standards = data;
+        if(this.standards.length > 0) {
+          this.onSelectionChange(this.standards[0]['standardId']);
+        }
+      },
+      (error) => {
+        this.spinnerService.hide();
+        swal({
+          position: 'center',
+          type: 'error',
+          title: 'Error occured in server',
+          showConfirmButton: false,
+          timer: 1500
+        });
+      }
+    )
   }
 
 }
